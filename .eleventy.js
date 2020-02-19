02d80fbf24f223e438a278b31cec850815d5dfc6
Timothy Miller <tim@webinspect.tech>
2020-02-19 16:35:12 -0600

    Add cleanURL filter along with boilerplate code

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+
+module.exports = function(eleventyConfig) {
+
+	eleventyConfig.addFilter('cleanURL', (url) => {
+		return new URL(url).hostname.replace("www.", "");
+	});
+
+	return {
+		templateFormats: [
+			"njk"
+		],
+		pathPrefix: "/"
+	};
+};
